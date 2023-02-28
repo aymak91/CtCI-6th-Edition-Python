29feaff88b74a42be2dc2b0ac6adbe467b11fba0
aymak91 <amakyu@gmail.com>
2023-02-27 22:50:03 -0800

    is unique

---
+++ chapter01/1.1_isUnique.py
@@ -0,0 +1,15 @@
+# Is Unique
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+def isUnique(s):
+    visited = set()
+
+    for letter in s:
+        if letter in visited: return False
+        visited.add(letter)
+    return True
+
+# O(n) where n is the length of the string
+
+# If we were not allowed to use additional data structures, we would have to compare every combination of letters. O(n^2)
+# If we were allowed to mutate the input, we can actually sort it and compare neighbors. O(nlogn)
