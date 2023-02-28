77272e157aa7ce59cb90d81d7fede77bb05ff566
aymak91 <amakyu@gmail.com>
2023-02-27 23:03:58 -0800

    update isUnique

--- chapter01/1.1_isUnique.py
+++ chapter01/1.1_isUnique.py
@@ -3,12 +3,23 @@
 def isUnique(s):
     visited = set()
 
-    for letter in s:
-        if letter in visited: return False
-        visited.add(letter)
+    for char in s:
+        if char in visited: return False
+        visited.add(char)
     return True
 
 # O(n) where n is the length of the string
 
-# If we were not allowed to use additional data structures, we would have to compare every combination of letters. O(n^2)
+# If we were not allowed to use additional data structures, we would have to compare every combination of chars. O(n^2)
 # If we were allowed to mutate the input, we can actually sort it and compare neighbors. O(nlogn)
+
+def isUnique(s):
+    s.sort()
+
+    last_char = None
+
+    for char in s:
+        if char == last_char: return False
+        last_char = char
+
+    return True
